570792665e6f22d5f2ee341de674294b634d888b
myl7 <myl@myl.moe>
2023-04-29 14:22:05 +0800

    Integrate with bench framework

--- benches/dmpf.rs
+++ benches/dmpf.rs
@@ -1,10 +1,6 @@
-#![feature(test)]
-
-extern crate test;
-
-use test::Bencher;
-
 use std::cell::RefCell;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use aes::cipher::generic_array::GenericArray;
 use aes::cipher::{BlockEncrypt, KeyInit};
@@ -102,63 +98,72 @@ impl BSampler for Sampler {
     }
 }
 
-#[bench]
-fn main(b: &mut Bencher) {
-    b.iter(|| {
-        let vdmpf = VDMPF::new(
-            80f64,
-            1000,
-            Box::new(PRP::default()),
-            Box::new(CHSampler::default()),
-            Box::new(Sampler::default()),
-            1000,
-            16,
-            Box::new(PRG::default()),
-            Box::new(Hash::default()),
-            Box::new(Hash::default()),
-            Box::new(Hash::default()),
-            Box::new(Sampler::default()),
-            1000,
-        );
-        let mut fs = Vec::with_capacity(100);
-        let mut f_rng = thread_rng();
-        for _ in 0..100 {
-            let mut a = vec![0; 16];
-            f_rng.fill_bytes(&mut a);
-            a[0] = 0x07;
-            let mut b = vec![0; 16];
-            f_rng.fill_bytes(&mut b);
-            fs.push(PointFn { a, b });
-        }
-        let fs_ref = fs.iter().collect::<Vec<_>>();
-        let mut mshare0 = vdmpf.gen(&fs_ref).unwrap();
-        let mut mshare1 = mshare0.clone();
-        mshare0
-            .ks
-            .iter_mut()
-            .for_each(|k| k.s0s = vec![k.s0s[0].clone()]);
-        mshare1
-            .ks
-            .iter_mut()
-            .for_each(|k| k.s0s = vec![k.s0s[1].clone()]);
+fn var_t(point_n: usize) {
+    let vdmpf = VDMPF::new(
+        80f64,
+        1000,
+        Box::new(PRP::default()),
+        Box::new(CHSampler::default()),
+        Box::new(Sampler::default()),
+        1000,
+        16,
+        Box::new(PRG::default()),
+        Box::new(Hash::default()),
+        Box::new(Hash::default()),
+        Box::new(Hash::default()),
+        Box::new(Sampler::default()),
+        1000,
+    );
+    let mut fs = Vec::with_capacity(point_n);
+    let mut f_rng = thread_rng();
+    for _ in 0..point_n {
+        let mut a = vec![0; 16];
+        f_rng.fill_bytes(&mut a);
+        a[0] = 0x07;
+        let mut b = vec![0; 16];
+        f_rng.fill_bytes(&mut b);
+        fs.push(PointFn { a, b });
+    }
+    let fs_ref = fs.iter().collect::<Vec<_>>();
+    let mut mshare0 = vdmpf.gen(&fs_ref).unwrap();
+    let mut mshare1 = mshare0.clone();
+    mshare0
+        .ks
+        .iter_mut()
+        .for_each(|k| k.s0s = vec![k.s0s[0].clone()]);
+    mshare1
+        .ks
+        .iter_mut()
+        .for_each(|k| k.s0s = vec![k.s0s[1].clone()]);
 
-        let mut xs = fs.iter().map(|f| f.a.clone()).collect::<Vec<_>>();
-        for _ in 0..100 {
-            let mut a = vec![0; 16];
-            f_rng.fill_bytes(&mut a);
-            a[0] = 0x0f;
-            xs.push(a);
+    let mut xs = fs.iter().map(|f| f.a.clone()).collect::<Vec<_>>();
+    for _ in 0..point_n {
+        let mut a = vec![0; 16];
+        f_rng.fill_bytes(&mut a);
+        a[0] = 0x0f;
+        xs.push(a);
+    }
+    let xs_ref = xs.iter().map(|x| x.as_ref()).collect::<Vec<_>>();
+    let (y0s, pi0) = vdmpf.eval(false, &mshare0, &xs_ref, fs.len());
+    let (y1s, pi1) = vdmpf.eval(true, &mshare1, &xs_ref, fs.len());
+    assert_eq!(vdmpf.verify(&[&pi0, &pi1]), true);
+    for ((x, y0), y1) in xs.iter().zip(y0s.iter()).zip(y1s.iter()) {
+        let y: Vec<u8> = (BGroup::from(y0.to_owned()) + y1.as_ref()).into();
+        match fs.iter().find(|f| f.a == *x) {
+            Some(f) => assert_eq!(y, f.b),
+            None => assert_eq!(y, hex!("00000000000000000000000000000000")),
         }
-        let xs_ref = xs.iter().map(|x| x.as_ref()).collect::<Vec<_>>();
-        let (y0s, pi0) = vdmpf.eval(false, &mshare0, &xs_ref, fs.len());
-        let (y1s, pi1) = vdmpf.eval(true, &mshare1, &xs_ref, fs.len());
-        assert_eq!(vdmpf.verify(&[&pi0, &pi1]), true);
-        for ((x, y0), y1) in xs.iter().zip(y0s.iter()).zip(y1s.iter()) {
-            let y: Vec<u8> = (BGroup::from(y0.to_owned()) + y1.as_ref()).into();
-            match fs.iter().find(|f| f.a == *x) {
-                Some(f) => assert_eq!(y, f.b),
-                None => assert_eq!(y, hex!("00000000000000000000000000000000")),
-            }
-        }
-    });
+    }
 }
+
+fn criterion_bench(c: &mut Criterion) {
+    c.bench_function("t 1000", |b| b.iter(|| var_t(black_box(1000))));
+}
+
+criterion_group! {
+    name = benches;
+    // This can be any expression that returns a `Criterion` object.
+    config = Criterion::default().sample_size(10);
+    targets = criterion_bench
+}
+criterion_main!(benches);
